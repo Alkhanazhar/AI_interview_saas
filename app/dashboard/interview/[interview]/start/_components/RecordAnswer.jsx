6e4239a6e7e67a31d55aebd5aa73375ec1016a62
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2024-07-21 02:50:07 +0530

    regenerate

--- app/dashboard/interview/[interview]/start/_components/RecordAnswer.jsx
+++ app/dashboard/interview/[interview]/start/_components/RecordAnswer.jsx
@@ -1,5 +1,5 @@
 "use client";
-import "regenerator-runtime/runtime";
+// import "regenerator-runtime/runtime";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
